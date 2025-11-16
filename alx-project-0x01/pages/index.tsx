a4d4b8d2b8484e05004e05d244ec6eb5027a25e2
SCCAMP <msprexy1@gmail.com>
2025-11-16 16:55:14 +0100

    project setup

---
+++ alx-project-0x01/pages/index.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-7xl font-thin">Welcome Page</h1>
+    </div>
+  );
+}
+export default Home;
